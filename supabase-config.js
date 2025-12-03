50c82d9223766574684ef12c5b801dca82164018
delicecake38-rgb <delicecake38@gmail.com>
2025-12-03 03:44:31 +0100

    Delete supabase-config.js

--- supabase-config.js
+++
@@ -1,6 +0,0 @@
-// supabase-config.js
-const SUPABASE_URL = 'https://ypxvnxddzgaopjdhclzj.supabase.co'
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlweHZueGRkemdhb3BqZGhjbHpqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjIzODk1OTUsImV4cCI6MjA3Nzk2NTU5NX0.bMhlMjgbxjCZRUbXn0-WoZZaeSOySSAoEPzmL9UwpCo'
-
-// Initialiser Supabase
-const supabase = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY)
